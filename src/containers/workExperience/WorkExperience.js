69cbd326301ef42e59934d334c7c2fd6d17430dd
Divya <dm.divya.mahajan@gmail.com>
2023-11-16 21:44:30 -0800

    image fix

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -4,7 +4,7 @@ import ExperienceCard from "../../components/experienceCard/ExperienceCard";
 import {workExperiences} from "../../portfolio";
 import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
-import ConflictManagementImage from "../../assets/images/Conflict_Management.avif";
+import ConflictManagementImage from "./Conflict_Management.avif";
 import ConflictResolutionStrategies from "./ConflictResolutionStrategies";
 
 export default function WorkExperience() {
